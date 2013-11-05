a7466832b0d1533f606bb6e7163894b7b1de6d89
timkao <timkao@sklteki-MacBook-Pro.local>
2013-11-05 11:24:52 +0800

    Initial Commit

---
+++ xmltest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  xmltest
+//
+//  Created by timkao on 13/11/5.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "xmltestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([xmltestAppDelegate class]));
+    }
+}
---
+++ xmltest/xmltestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xmltestAppDelegate.h
+//  xmltest
+//
+//  Created by timkao on 13/11/5.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xmltestAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
