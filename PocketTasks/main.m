13adab591a00b6691af1b6639dddae2a5de97b96
timkao <timkao@sklteki-MacBook-Pro.local>
2013-11-06 11:15:09 +0800

    Initial Commit

---
+++ PocketTasks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PocketTasks
+//
+//  Created by timkao on 13/11/6.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PocketTasksAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PocketTasksAppDelegate class]));
+    }
+}
---
+++ PocketTasksTests/PocketTasksTests.m
@@ -0,0 +1,34 @@
+//
+//  PocketTasksTests.m
+//  PocketTasksTests
+//
+//  Created by timkao on 13/11/6.
+//  Copyright (c) 2013年 skl. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PocketTasksTests : XCTestCase
+
+@end
+
+@implementation PocketTasksTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
